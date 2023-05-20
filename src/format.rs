f02aaccc7a6b31374545489b823e8d5efb3b73ca
ghost-str <Ghost-str@users.noreply.github.com>
2023-05-20 11:37:14 +0300

    fix test

--- src/format.rs
+++ src/format.rs
@@ -1,6 +1,6 @@
 use crate::types::Event;
 use crate::types::{EventState, Participant};
-use chrono::{DateTime, Local, NaiveDateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 
 use crate::db;
 use db::EventStats;
@@ -10,8 +10,7 @@ use r2d2_sqlite::SqliteConnectionManager;
 pub fn ts(ts: u64) -> String {
     let naive = NaiveDateTime::from_timestamp_opt(ts as i64, 0).unwrap();
     let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
-    let local: DateTime<Local> = datetime.into();
-    local.format("%d.%m %H:%M").to_string()
+    datetime.format("%d.%m %H:%M").to_string()
 }
 
 pub fn event_title(event: &Event) -> String {
@@ -152,5 +151,5 @@ pub fn messages(
 
 #[test]
 fn test_format() {
-    assert_eq!(ts(1650445814), "20.04 12:10");
+    assert_eq!(ts(1650445814), "20.04 09:10");
 }
