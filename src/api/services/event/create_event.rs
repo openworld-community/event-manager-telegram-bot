fcbf79101b81bed3aa22d22094bd12598f3dedb2
kosmos-954 <tired.d.albert@gmail.com>
2023-05-30 12:48:41 +0600

    Trying fix create event

--- src/api/services/event/create_event.rs
+++ src/api/services/event/create_event.rs
@@ -3,6 +3,7 @@ use crate::api::shared::{into_internal_server_error_responce, QueryError};
 use crate::api::utils::json_responce;
 use crate::db::mutate_event;
 use crate::types::{DbPool, Event};
+use crate::admin_message_handler::add_event;
 use actix_web::http::StatusCode;
 use actix_web::web::{Data, Json};
 use actix_web::{post, Responder};
@@ -17,7 +18,7 @@ pub async fn create_event(
 
     let mut event: Event = event_to_create.into_inner().into();
     let cloned = event.clone();
-    let event_id = spawn_blocking(move || insert_event(&pool, &cloned))
+    let event_id = spawn_blocking(move || add_event(&pool, &cloned))
         .await
         .map_err(into_internal_server_error_responce)?
         .map_err(into_internal_server_error_responce)?;
@@ -26,26 +27,8 @@ pub async fn create_event(
 
     Ok(json_responce(&event, StatusCode::CREATED))
 }
-
-fn insert_event(pool: &DbPool, event: &Event) -> Result<u64, QueryError> {
-    let con = pool.get()?;
-    if event.adult_ticket_price != 0 && event.max_adults == 0
-        || event.child_ticket_price != 0 && event.max_children == 0
-    {
-        return Err(anyhow!("Wrong event format"));
-    }
-    match crate::db::mutate_event(conn, &event) {
-        Ok(id) => {
-            return Ok(ReplyMessage::new(if id > 0 {
-                let bot_name = env::var("BOT_NAME").unwrap();
-                format!("Direct event link: https://t.me/{}?start={}", bot_name, id)
-            } else {
-                format!("Failed to add event.")
-            })
-                .into());
-        }
-        Err(e) => {
-            return Err(anyhow!("Failed to add event: {}.", e));
-        }
-    }
-}
+//
+// fn insert_event(pool: &DbPool, event: &Event) -> Result<u64, QueryError> {
+//     let con = pool.get()?;
+//     Ok(mutate_event(&con, &event)?)
+// }
