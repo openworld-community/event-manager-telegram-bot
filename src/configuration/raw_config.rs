a90af390256e3dea9e767ededdb680d6acba0093
ghost-str <Ghost-str@users.noreply.github.com>
2023-05-31 03:14:11 +0300

    clean all space symbols

--- src/configuration/raw_config.rs
+++ src/configuration/raw_config.rs
@@ -1,4 +1,5 @@
 use chrono::DateTime;
+use regex::Regex;
 use std::collections::HashSet;
 use std::net::{SocketAddr, ToSocketAddrs};
 
@@ -27,13 +28,15 @@ pub struct RawConfiguration {
 
 impl RawConfiguration {
     pub fn parse_mailing_hours(&self) -> Result<(u64, u64), String> {
-        let parts: Vec<&str> = self.mailing_hours.split("..").collect();
+        let clean_all_space_symbols = Regex::new(r"\s+").unwrap();
+        let clean_space = clean_all_space_symbols.replace_all(self.mailing_hours.as_str(), " ");
+        let parts: Vec<&str> = clean_space.split("..").map(|part| part.trim()).collect();
         if parts.len() != 2 {
             return Err("Wrong mailing hours format.".to_string());
         }
         match (
-            DateTime::parse_from_str(&format!("2022-07-06 {}", parts[0]), "%Y-%m-%d %H:%M  %z"),
-            DateTime::parse_from_str(&format!("2022-07-06 {}", parts[1]), "%Y-%m-%d %H:%M  %z"),
+            DateTime::parse_from_str(&format!("2022-07-06 {}", parts[0]), "%Y-%m-%d %H:%M %z"),
+            DateTime::parse_from_str(&format!("2022-07-06 {}", parts[1]), "%Y-%m-%d %H:%M %z"),
         ) {
             (Ok(from), Ok(to)) => {
                 let mailing_hours_from = (from.timestamp() % 86400) as u64;
