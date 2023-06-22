a01fccf2ae3eae33ca15bbf64ba188e37c7354a3
Dmitry <Ghost-str@users.noreply.github.com>
2023-06-22 12:37:56 +0300

    Cors middleware (#32)

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,4 @@
+mod middlewares;
 mod services;
 mod shared;
 mod utils;
@@ -6,6 +7,7 @@ use crate::api::services::event::event_scope;
 use crate::types::DbPool;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use middlewares::cors_middleware;
 use std::net::ToSocketAddrs;
 
 pub fn setup_api_server<Addr: ToSocketAddrs>(addr: &Addr, con_pool: &DbPool) -> Server {
@@ -13,6 +15,7 @@ pub fn setup_api_server<Addr: ToSocketAddrs>(addr: &Addr, con_pool: &DbPool) -> 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .wrap(cors_middleware())
             .service(event_scope())
     })
     .bind(&addr)
