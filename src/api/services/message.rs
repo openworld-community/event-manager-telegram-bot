4f810653525dc22c2c10c989ee5ba765c5479b80
ghost-str <Ghost-str@users.noreply.github.com>
2023-07-15 12:37:28 +0300

    create message

--- src/api/services/message.rs
+++ src/api/services/message.rs
@@ -1,7 +1,10 @@
-use entity::message::{Column, Entity};
+use crate::api::services::message_outbox::create_message_outbox;
+use chrono::Utc;
+use entity::message::{ActiveModel, Column, Entity};
 use entity::new_types::MessageType;
+use entity::{message, message_outbox};
 use sea_orm::prelude::*;
-use sea_orm::DeleteResult;
+use sea_orm::{ActiveValue, DeleteResult};
 
 pub async fn delete_enqueued_messages<C>(
     event_id: &i32,
@@ -21,4 +24,40 @@ where
         .await?;
 
     Ok(res.rows_affected)
+}
+
+#[derive(Debug)]
+pub struct ResultCreateMessage {
+    message: message::Model,
+    message_outbox: message_outbox::Model,
+}
+
+pub async fn create_message<C>(
+    event_id: i32,
+    sender: &str,
+    waiting_list: i32,
+    message_type: MessageType,
+    text: &str,
+    send_at: chrono::DateTime<Utc>,
+    con: &C,
+) -> Result<ResultCreateMessage, DbErr>
+where
+    C: ConnectionTrait,
+{
+    let ac = ActiveModel {
+        id: ActiveValue::NotSet,
+        event: ActiveValue::Set(event_id),
+        r#type: ActiveValue::Set(message_type),
+        sender: ActiveValue::Set(sender.to_string()),
+        waiting_list: ActiveValue::Set(waiting_list),
+        text: ActiveValue::Set(text.to_string()),
+        ts: ActiveValue::Set(Utc::now().naive_utc()),
+    };
+
+    let message = ac.insert(con).await?;
+
+    Ok(ResultCreateMessage {
+        message_outbox: create_message_outbox(message.id, send_at, con).await?,
+        message,
+    })
 }
