b5123a158a1fa5168bfd607826730d3842e4c339
kosmos-954 <tired.d.albert@gmail.com>
2023-08-02 00:31:01 +0600

    Added postgres configuration

--- src/configuration/config.rs
+++ src/configuration/config.rs
@@ -22,19 +22,18 @@ pub struct Config {
     pub mailing_hours_from: u64,
     pub mailing_hours_to: u64,
     pub api_socket_address: SocketAddr,
-    pub db_url: String,
+    pub db_connection_string: String,
 }
 
 impl From<RawConfiguration> for Config {
     fn from(value: RawConfiguration) -> Self {
         let mailing_hours = value.parse_mailing_hours().unwrap();
-        let db_url = format!(
-            "{}://{}:{}@{}:{}/{}",
-            value.db_protocol,
-            value.db_user,
-            value.db_password,
+        let db_connection_string = format!(
+            "host={} port={} user={} password={} dbname={}"
             value.db_host,
             value.db_port,
+            value.db_user,
+            value.db_password,
             value.db_name
         );
         Config {
@@ -57,7 +56,7 @@ impl From<RawConfiguration> for Config {
             limit_bulk_notifications_per_second: value.limit_bulk_notifications_per_second,
             mailing_hours_from: mailing_hours.0,
             mailing_hours_to: mailing_hours.1,
-            db_url,
+            db_connection_string,
         }
     }
 }
