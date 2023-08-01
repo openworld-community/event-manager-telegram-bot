3a4e7904d15028364083f312d7df0d38fe393538
kosmos-954 <tired.d.albert@gmail.com>
2023-08-02 05:10:58 +0600

    Trying to fix type for connection

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use deadpool_postgres::{Manager, ManagerConfig, Pool, PoolError, RecyclingMethod};
+use deadpool_postgres::{Connection, Manager, ManagerConfig, Pool, PoolError, RecyclingMethod};
 // use r2d2::PooledConnection;
 // use r2d2_sqlite::SqliteConnectionManager;
 use serde_compact::compact;
@@ -10,7 +10,7 @@ use crate::configuration::config::Config;
 use teloxide::types::UserId;
 
 pub type DbPool = deadpool_postgres::Pool;
-//pub type Connection = deadpool::Client<Manager>;
+pub type Connection = deadpool::Connection<Manager>;
 //pub type EventId = u64;
 
 #[derive(PartialEq)]
