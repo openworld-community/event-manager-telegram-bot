092c234b489ee1a63819df34750b694492f72207
kosmos-954 <tired.d.albert@gmail.com>
2023-08-02 12:18:16 +0600

    linter fixes 3

--- src/api/services/event/create_event.rs
+++ src/api/services/event/create_event.rs
@@ -1,7 +1,7 @@
 use crate::api::services::event::types::RawEvent;
 use crate::api::shared::{into_internal_server_error_response, QueryError};
 use crate::api::utils::json_response;
-use crate::db::mutate_event;
+use crate::db::{mutate_event, row_to_u64};
 use crate::types::{DbPool, Event};
 use actix_web::http::StatusCode;
 use actix_web::web::{Data, Json};
@@ -21,7 +21,7 @@ pub async fn create_event(
         .await
         .map_err(into_internal_server_error_response)?;
 
-    event.id = event_id;
+    event.id = row_to_u64(event_id);
 
     Ok(json_response(&event, StatusCode::CREATED))
 }
